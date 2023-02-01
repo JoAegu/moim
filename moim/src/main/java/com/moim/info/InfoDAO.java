0d87555345a78887566c1562dc7e7b95c6dcf80b
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-02-01 12:08:07 +0900

    Day04 moimWrite

--- moim/src/main/java/com/moim/info/InfoDAO.java
+++ moim/src/main/java/com/moim/info/InfoDAO.java
@@ -3,6 +3,8 @@ package com.moim.info;
 import java.sql.*;
 import java.util.*;
 
+import com.moim.noimg.NoimgDTO;
+
 public class InfoDAO {
 	Connection conn;
 	PreparedStatement ps;
@@ -12,5 +14,40 @@ public class InfoDAO {
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**모임글 본문보기 관련 메서드 */
+	public NoimgDTO getContent(int idx) {
+		try {
+			conn=com.moim.db.MoimDB.getConn();
+			String sql="select * from moim_noimg where idx=? and category=2";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, idx);
+			rs=ps.executeQuery();
 
+			NoimgDTO dto=null;
+			if(rs.next()) {
+				int idx_member=rs.getInt("idx_member");
+				int idx_info=rs.getInt("idx_info");
+				int category=rs.getInt("category");
+				String writer=rs.getString("writer");
+				String subject=rs.getString("subject");
+				String content=rs.getString("content");
+				java.sql.Date writedate=rs.getDate("writedate");
+				int ref=rs.getInt("ref");
+				int lev=rs.getInt("lev");
+				int sunbun=rs.getInt("sunbun");
+				
+				dto=new NoimgDTO(idx, idx_member, idx_info, category,writer, subject, content, writedate, ref, lev, sunbun);
+			}
+			return dto;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null)ps.close();
+				if(conn!=null)conn.close();
+			} catch (Exception e2) {}
+		}
+	}
 }
---
+++ moim/src/main/webapp/info/moimWrite_ok.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%
+String category=request.getParameter("category");
+String moimname=request.getParameter("moimname");
+String content=request.getParameter("content");
+String local=request.getParameter("local");
+%>
